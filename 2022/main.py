540dd849dd5d9598c43905d2f755501eb984b89e
Dominik Terweh <d.terweh@drooms.com>
2022-12-09 14:55:44 +0100

    added days 7 through 9

--- 2022/main.py
+++ 2022/main.py
@@ -178,9 +178,213 @@ def day06(filename):
     print(f"day 06, part 2: {solution_2}")
 
 
+def day07(filename):
+    from os import path
+    from collections import defaultdict
+    solution_1 = 0
+    solution_2 = 0
+    current_path = "/"
+    dirs = defaultdict(list)
+    with open(filename) as f:
+        for line in f:
+            line = line.strip("\n")
+            if line[0] == "$":
+                if line[2:4] == "cd":
+                    if line[5:] == "..":
+                        current_path = path.dirname(current_path)
+                    else:
+                        current_path = path.join(current_path, line[5:])
+                if line[2:4] == "ls":
+                    continue
+            elif line[:3] == "dir":
+                directory = path.join(current_path, line[4:])
+                dirs[current_path].append(directory)
+                dirs[directory]
+            else:
+                size, _ = line.split(" ")
+                dirs[current_path].append(int(size))
+
+    def get_size(key, dir_dict):
+        sum = 0
+        for value in dir_dict[key]:
+            if isinstance(value, int):
+                sum += value
+            else:
+                sum += get_size(value, dir_dict)
+        return sum
+
+    for key in dirs:
+        dirs[key] = get_size(key, dirs)
+
+    for key in dirs:
+        if dirs[key] < 100000:
+            solution_1 += dirs[key]
+
+    # dirs_space = dirs["/"]
+    # disk_space = 70000000
+    # update_space = 30000000
+    # free_space = disk_space - dirs_space
+    # needed_space = update_space - free_space
+    needed_space = 30000000 - (70000000 - dirs["/"])
+    solution_2 = dirs["/"]
+    for key in dirs:
+        if dirs[key] > needed_space and dirs[key] < solution_2:
+            solution_2 = dirs[key]
+
+    print(f"day 07, part 1: {solution_1}")
+    print(f"day 07, part 2: {solution_2}")
+
+
+def day08(filename):
+    def check_visibility(grid, x, y):
+        line = grid[y]
+        column = [grid[i][x] for i in range(0, len(grid))]
+        value = grid[y][x]
+        left = max(line[:x])
+        right = max(line[x+1:])
+        top = max(column[:y])
+        bottom = max(column[y+1:])
+        if min([left, right, top, bottom]) < value:
+            return 1
+        return 0
+
+    def find_score(grid, x, y):
+        line = grid[y]
+        column = [grid[i][x] for i in range(0, len(grid))]
+        value = grid[y][x]
+        left, right = line[:x], line[x+1:]
+        left.reverse()
+        top, bottom = column[:y], column[y+1:]
+        top.reverse()
+        score = (
+            get_sight(left, value) * get_sight(right, value) *
+            get_sight(top, value) * get_sight(bottom, value)
+            )
+        return score
+
+    def get_sight(trees, value):
+        # sight = trees
+        for i, v in enumerate(trees):
+            if v >= value:
+                return i+1
+
+    solution_2 = 0
+    treegrid = []
+    with open(filename) as f:
+        for line in f:
+            treegrid.append(list(line.strip("\n")))
+
+    width = len(treegrid[0])
+    height = len(treegrid)
+    solution_1 = 2*width + 2*height - 4  # remove corners, counted twice
+
+    for x in range(1, width-1):
+        for y in range(1, height-1):
+            solution_1 += check_visibility(treegrid, x, y)
+
+    for x in range(1, width-1):
+        for y in range(1, height-1):
+            score = find_score(treegrid, x, y)
+            if score > solution_2:
+                solution_2 = score
+
+    print(f"day 08, part 1: {solution_1}")
+    print(f"day 08, part 2: {solution_2}")
+
+
+def day09(filename):
+    solution_2 = 0
+    loc_H = {"x": 0, "y": 0}
+    loc_T = {"x": 0, "y": 0}
+    location_bag = set([str(loc_H)])
+
+    def move(loc, dir, stp):
+        #         U +y
+        #         ∧
+        # L -x <- s -> R +x
+        #         v
+        #         D -y
+        instructions = {
+            "L": ["x", -1],
+            "R": ["x", 1],
+            "U": ["y", -1],
+            "D": ["y", 1]
+        }
+        axis, factor = instructions[dir]
+        loc[axis] += stp*factor
+        return loc
+
+    def adjust(loc_T, loc_H, adding):
+        x_dist = loc_H["x"] - loc_T["x"]
+        y_dist = loc_H["y"] - loc_T["y"]
+
+        if abs(x_dist) < 2 and abs(y_dist) < 2:
+            if adding:
+                location_bag.add(str(loc_T))
+            return loc_T
+
+        if x_dist > 1 or x_dist > 0 and abs(y_dist) > 1:
+            loc_T["x"] += 1
+        elif x_dist < -1 or x_dist < 0 and abs(y_dist) > 1:
+            loc_T["x"] -= 1
+
+        if y_dist > 1 or y_dist > 0 and abs(x_dist) > 1:
+            loc_T["y"] += 1
+        elif y_dist < -1 or y_dist < 0 and abs(x_dist) > 1:
+            loc_T["y"] -= 1
+
+        if adding:
+            location_bag.add(str(loc_T))
+        return adjust(loc_T, loc_H, adding)
+
+    with open(filename) as f:
+        for line in f:
+            line = line.strip("\n")
+            direction, steps = line.split(" ")
+            loc_H = move(loc_H, direction, int(steps))
+            loc_T = adjust(loc_T, loc_H, True)
+
+    solution_1 = len(location_bag)
+
+    loc_H = {"x": 0, "y": 0}
+    location_bag = set([str(loc_H)])
+    tails = {i: {"x": 0, "y": 0} for i in range(10)}
+    print(tails)
+    with open(filename) as f:
+        for line in f:
+            line = line.strip("\n")
+            direction, steps = line.split(" ")
+            for _ in range(int(steps)):
+                loc_H = move(loc_H, direction, 1)
+                prev = loc_H
+                for tail in tails:
+                    adding = True if tail == 8 else False
+                    tails[tail] = adjust(tails[tail], prev, adding)
+                    prev = tails[tail]
+
+    solution_2 = len(location_bag)
+
+    print(f"day 09, part 1: {solution_1}")
+    print(f"day 09, part 2: {solution_2}")
+
+
 # day01("2022/day01.txt")
 # day02("2022/day02.txt")
 # day03("2022/day03.txt")
 # day04("2022/day04.txt")
 # day05("2022/day05.txt")
-day06("2022/day06.txt")
+# day06("2022/day06.txt")
+# day07("2022/day07.txt")
+# day08("2022/day08.txt")
+day09("2022/day09.txt")
+
+
+def dayXX(filename):
+    solution_1 = 0
+    solution_2 = 0
+    with open(filename) as f:
+        for line in f:
+            continue
+
+    print(f"day XX, part 1: {solution_1}")
+    print(f"day XX, part 2: {solution_2}")
